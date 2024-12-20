3c4c6dd858419533bcf207d754be1594e512dc62
jonathan-alegre <alegrejonathan.93@gmail.com>
2024-12-19 21:37:34 -0300

    Custom Services - Injectable Decorator

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -13,6 +13,7 @@ import {
 } from '@angular/core';
 import {Course} from '../model/course';
 import {CourseImageComponent} from '../course-image/course-image.component';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
     selector: 'course-card',
@@ -32,12 +33,12 @@ export class CourseCardComponent implements OnInit {
     courseEmitter = new EventEmitter<Course>();
 
 
-    constructor() {
+    constructor(private coursesService: CoursesService) {
 
     }
 
     ngOnInit() {
-
+        console.log("Couses service card", this.coursesService);
     }
 
 
