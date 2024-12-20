4eaa83bbdc7cf3cebb13c76f2b783ccb7c0de85f
jonathan-alegre <alegrejonathan.93@gmail.com>
2024-12-19 21:44:10 -0300

    Custom Service - Fetching Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,35 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { CoursesService } from './services/courses.service';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { HighlightedDirective } from "./directives/highlighted.directive";
+import { Observable } from "rxjs";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { CoursesService } from "./services/courses.service";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  standalone: false,
 })
 export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-  courses$: Observable<Course[]>;  
-
-  constructor(private http: HttpClient, private coursesService: CoursesService) {
-
-  }
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
 
-    console.log(this.coursesService);
-
-    const params =  new HttpParams()
-      .set("page", "1")
-      .set("pageSize", "10");
-
-    this.courses$ = this.http.get<Course[]>('api/courses', {params});                 
-
+    this.courses$ = this.coursesService.loadCourses();
+  
   }
-
-
 
 }
